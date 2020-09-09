88126e9248f185fee721095c1055f49bef2ed539
kida <806589617@qq.com>
2020-09-09 19:50:55 +0800

    RedisManager

---
+++ MVCPractice.Cache/Redis/RedisManager.cs
@@ -0,0 +1,31 @@
+﻿using MVCPractice.Cache.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVCPractice.Cache.Redis
+{
+    class RedisManager:ICache
+    {
+        public RedisManager()
+        {
+
+        }
+
+        private RedisManager _instance;
+
+        public RedisManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+            set
+            {
+                
+            }
+        }
+
+        
+    }
+}
